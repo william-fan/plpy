6000fb85e254f6a63234ee850079144458fe81b3
William Fan <william.fan1@student.unsw.edu.au>
2016-09-28 22:01:16 +1000

    passes 0-2

--- plpy.pl
+++ plpy.pl
@@ -4,6 +4,7 @@
 # http://cgi.cse.unsw.edu.au/~cs2041/assignments/plpy/
 
 @final = ();
+@imports = ();
 
 if (scalar @ARGV == 0){
 	open(F,"-");
@@ -14,16 +15,22 @@ if (scalar @ARGV == 0){
 while ($line = <F>) {
 	chomp ($line);
 	$line = header($line);
+	$line = strip($line);
+	$line = stdin($line);
 	$line = comments($line);
 	$line = printing($line);
 	$line = variables($line);
+	$line = skips($line);
 	$line = semicolons($line);
 	$line = braces($line);
 	#$line = unknown($line);
 	if ($line ne ""){
 		push @final, $line;
 	}
+	
 }
+
+unshift @final, @imports;
 
 foreach $arg (@final){
 	print "$arg\n";
@@ -31,8 +38,26 @@ foreach $arg (@final){
 
 sub header {	# translate #! line 
 	if ($_[0] =~ m/^#!/ && $. == 1) {
-		$_[0] = "#!/usr/local/bin/python3.5 -u"
+		push @imports, "#!/usr/local/bin/python3.5 -u";
+		$_[0] = "";
 	}
+	return $_[0];
+}
+
+sub strip {
+	if ($_[0] =~ /chomp (.*);/) {
+		$_[0] =~ s/chomp (.*);/$1 = $1.rstrip()/;
+	}
+	return $_[0];
+
+}
+
+sub stdin {
+	if ($_[0] =~ /<STDIN>/) {
+		$_[0] =~ s/<STDIN>/sys.stdin.readline()/;
+		imports("import sys");
+	}
+
 	return $_[0];
 }
 
@@ -62,6 +87,13 @@ sub variables {
 	return $_[0];
 }
 
+sub skips {
+	if ($_[0] =~ /last;$/){
+		$_[0] =~ s/last;$/break/;
+	}
+	return $_[0];
+}
+
 sub semicolons {
 	if ($_[0] =~ /.*;$/){	#converts all $ right now
 		$_[0] =~ s/;$//;
@@ -69,14 +101,30 @@ sub semicolons {
 	return $_[0];
 }
 
-sub braces {	#might switch to handle specific loops.
-	if ($_[0] =~ /\s*{$/){
-		$_[0] =~ s/\s*{$/:/;
+sub braces {	#replaces braces with colons
+	if ($_[0] =~ /\((.*) eq (.*)\)\s*{$/){
+		$_[0] =~ s/\((.*) eq (.*)\)\s*{$/$1 == $2:/;
 	}
+	elsif ($_[0] =~ /\((.*)\)\s*{$/){
+		$_[0] =~ s/\((.*)\)\s*{$/$1:/;	#replaces parentheses in conditionals #maybe replace for advanced 
+	} 
 	if 	($_[0] =~ /}$/){
 		$_[0] =~ s/}$//;
 	}
 	return $_[0];
+}
+
+sub imports { #check if already imported
+	$temp = 0;
+	foreach $arg (@imports) {
+		if ($arg eq $_[0]) {
+			$temp = 1;
+		}
+	}
+	if ($temp == 0) {
+		push @imports, $_[0];
+	}
+	return;
 }
 
 sub unknown {	# Lines we can't translate are turned into comments
--- test00.pl
+++ test00.pl
@@ -1,6 +1,14 @@
-#!/usr/bin/perl
-$x = 1;
-while ($x < 5) {
-    print "$x\n";
-    $x = $x + 1;
+#!/usr/bin/perl -w
+
+while (1) {
+    print "Give me cookie\n";
+    $line = <STDIN>;
+    chomp $line;
+    if ($line eq "cookie") {
+        last;
+    }
 }
+print "Thank you\n";
+<STDIN>;
+<STDIN>;
+<STDIN>;
